235bfa21577974a00977c8518a3c56c3cadbbcdf
RahafZourob <zourobrahaf@gmail.com>
2023-07-02 23:06:45 +0300

    add counter to production

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
+      <Counter num={0} amount={2} />
+      <Counter num={2} amount={2} />
+      <Counter num={3} amount={2} />
 
     </div>
   );
