93a8d22283c420652445a7e5bbe9232e8855e76c
RahafZourob <zourobrahaf@gmail.com>
2023-07-05 17:43:23 +0300

    Add data to counter

---
+++ src/components/mock/CounterData/index.jsx
@@ -0,0 +1,17 @@
+export const counterdata = [
+  {
+      id:1,
+      num:0,
+      amount:1
+  },
+  {
+      id:2,
+      num:0,
+      amount:1
+  },
+  {
+      id:3,
+      num:0,
+      amount:1
+  }
+]
