9b98d2624429f2b2ad0eb8a9aeeec57fd70926bb
RahafZourob <zourobrahaf@gmail.com>
2023-07-08 02:37:10 +0300

    edit bugs

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -5,7 +5,7 @@ class Counter extends Component {
     render(){
         const{increment,decrement,num,id,amount}=this.props
     return (
-      <div>
+      <div className="counter">
         <span>{num}</span>
         <button className="increment" onClick={()=>increment(id,amount)}>+</button>
         <button className="decrement" onClick={()=>decrement(id,amount)}>-</button>
