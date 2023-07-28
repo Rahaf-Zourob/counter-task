b8160ed4e1eeb7840d2b1c59ead7a159b4be1e80
RahafZourob <zourobrahaf@gmail.com>
2023-07-28 17:11:46 +0300

    Delete comment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-// import Todos from "./components/Counter";
-// function App() {
-//   return(
-//   <div>
-// <Todos />
-//   </div>
-//   );
-// }
-
-// export default App;
 import Todos from "../src/components/Todos";
-
 function App() {
   return (
     <div className="App">
